538a9457d43db0899974d4d6a3879d5a65df188d
codleto <codleto@gmail.com>
2025-10-06 16:53:24 +0300

    refactor: rename variables and methods for clarity

--- src/Stars.java
+++ src/Stars.java
@@ -19,97 +19,90 @@ import java.util.Scanner;
 public class Stars {
     public static void main(String[] args) {
         do {
-            startOfTheGame();
-
+            showStartMenu();
         } while (true);
-
-
     }
 
-    public static void game(){
+    public static void start() {
         do {
-            show();
+            showBoard();
             String s = scanner();
             chekUp(s);
-            winGame();
-            loseGame();
+            checkWin();
+            checkLoss();
         } while (true);
     }
 
-    public static void startOfTheGame() {
+    public static void showStartMenu() {
+        //Меню в начале игры
         System.out.println("1 - Новая игра");
         System.out.println("2 - Статистика");
-        Scanner inputChose = new Scanner(System.in);
-        int newGame = inputChose.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int menuChoice = scanner.nextInt();
         do {
-            if (newGame == 1) {
-                restart();
-
-                game();
-            }
-            else if(newGame == 2) {
-                if (win == 0 && lose == 0){
+            if (menuChoice == 1) { // если выбрали 1 - начинаем новую игру
+                startNewGame();
+                start();
+            } else if (menuChoice == 2) { // если 2 - смотрим статистику
+                if (wins == 0 && losses == 0) { //проверяем счетчики
                     System.out.println("Ты еще не сыграл, статистики нет ;)");
                     System.out.println("0 - Выход");
-                    Scanner exit = new Scanner(System.in);
-                    int exi;
+                    Scanner scanner1 = new Scanner(System.in);
+                    int exit;
                     do {
-                        exi = exit.nextInt();
-                        if(exi != 0) {
+                        exit = scanner1.nextInt();
+                        if (exit != 0) {
                             System.out.println("Неверный ввод");
                         }
-                    }while(exi != 0);
-                    startOfTheGame();
+                    } while (exit != 0);
+                    showStartMenu();
                 } else {
                     System.out.println("Твоя статистика!");
-                    System.out.println("Выигрыши: " + win);
-                    System.out.println("Проигрыши: " + lose);
+                    System.out.println("Выигрыши: " + wins);
+                    System.out.println("Проигрыши: " + losses);
                     System.out.println("0 - Выход");
-                    Scanner exit = new Scanner(System.in);
-                    int exi;
+                    Scanner scanner1 = new Scanner(System.in);
+                    int exit;
                     do {
-                        exi = exit.nextInt();
-                    if(exi != 0) {
-                        System.out.println("Неверный ввод");
-                    }
-                    }while(exi != 0);
-                    startOfTheGame();
-
+                        exit = scanner1.nextInt();
+                        if (exit != 0) {
+                            System.out.println("Неверный ввод");
+                        }
+                    } while (exit != 0);
+                    showStartMenu();
                 }
-
             } else {
                 System.out.println("Ошибка ввода! Введите нужную цифру (1 или 2)");
             }
         } while (true);
     }
 
-    public static void winGame() {
-        if(word.length() == good) {
+    public static void checkWin() { // проверка выиграли мы или нет
+        if (secretWord.length() == correctLettersCount) {
             System.out.println("ТЫ ВЫИГРАЛ!!!");
-            win++;
-            startOfTheGame();
+            wins++;
+            showStartMenu();
         }
     }
 
-    public static void loseGame() {
-        if(initial == 6){
-            show();
+    public static void checkLoss() {  //проверка проиграли мы или нет
+        if (mistakesCount == 6) {
+            showBoard();
             System.out.println("Вы проиграли!");
-            lose++;
-            startOfTheGame();
+            losses++;
+            showStartMenu();
         }
-
     }
 
     //СЧЕТЧИКИ
-    public static int win = 0; // счетчик выигрышей
-    public static int lose = 0; // счетчик проигрышей
-    public static int initial = 0; // счетчик ошибок
-    public static int nev = 1; //счетчик неверных букв
-    public static int good = 0; // счетчик отгаданных букв
+    public static int wins = 0; // счетчик выигрышей
+    public static int losses = 0; // счетчик проигрышей
+    public static int mistakesCount = 0; // счетчик ошибок
+    public static int wrongLettersCount = 1; //счетчик неверных букв
+    public static int correctLettersCount = 0; // счетчик отгаданных букв
 
     // Поле
-    static String[][] playingField = {{"__", "__", "_ "},
+    static String[][] board = {{"__", "__", "_ "},
             {"| ", "  ", " |"},
             {"| ", "  ", "  "},
             {"| ", "  ", "   "},
@@ -119,23 +112,22 @@ public class Stars {
             {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
             {"Неверно введенные буквы :", "", "", "", "", "", ""}};
     //Список слов
-    static String[] collectionWord = {"Кукуруза", "Болото", "Автомобиль", "Чебурек", "Жираф", "Бамбук"};
+    static String[] collectionWords = {"Кукуруза", "Болото", "Автомобиль", "Чебурек", "Жираф", "Бамбук"};
 
     // рандомное слово
-    static String word = randomWord();
+    static String secretWord = getRandomWord();
 
-
-    public static String randomWord() {
+    public static String getRandomWord() {
         Random random = new Random();
-        int randomWord = random.nextInt(collectionWord.length);
-        String chooseWord = collectionWord[randomWord];
+        int randomWord = random.nextInt(collectionWords.length);
+        String chooseWord = collectionWords[randomWord];
 
         // показать сколько букв в слове
         int lengthWord = chooseWord.length();
 
         // вставка количества букв в виде "_" в поле
         for (int i = 0; i < lengthWord; i++) {
-            playingField[7][i] = "_ ";
+            board[7][i] = "_ ";
         }
         return chooseWord;
     } // // Вытаскиваем рандомное слово из списка и делаем под него количество ячеек в поле
@@ -145,24 +137,21 @@ public class Stars {
         int i = 0;
         int e = 0;
         String x = a.substring(0, 1);
-        while (i < word.length()) {
-            String y = word.substring(i, i + 1);
+        while (i < secretWord.length()) {
+            String y = secretWord.substring(i, i + 1);
             if (x.equalsIgnoreCase(y)) {
-                playingField[7][i] = y + " ";
-                good++;
+                board[7][i] = y + " ";
+                correctLettersCount++;
                 e++;
             }
             i++;
-
         }
         if (e == 0) {
-            playingField[8][nev] = x + " ";
-            initial++;
-            error(initial);
-            nev++;
+            board[8][wrongLettersCount] = x + " ";
+            mistakesCount++;
+            incrementMistakes(mistakesCount);
+            wrongLettersCount++;
         }
-
-
     }  // проверка есть ли такая буква в слове и вывод неверных букв
 
     public static String scanner() {
@@ -172,63 +161,57 @@ public class Stars {
             String sc = scanner.nextLine();
             if (sc.length() != 1) {
                 System.out.println("Вы ввели больше одной буквы");
-                continue;
             } else {
                 return sc;
             }
         } while (true);
     } // ввод и проверка кол-во букв
 
-    public static void error(int a) {
+    public static void incrementMistakes(int a) {
         switch (a) {
             case 1:
-                playingField[2][2] = " 0";
+                board[2][2] = " 0";
                 break;
             case 2:
-                playingField[3][2] = " |";
+                board[3][2] = " |";
                 break;
             case 3:
-                playingField[3][2] = "/|";
+                board[3][2] = "/|";
                 break;
             case 4:
-                playingField[3][2] = "/|\\";
+                board[3][2] = "/|\\";
                 break;
             case 5:
-                playingField[4][2] = " /";
+                board[4][2] = " /";
                 break;
             case 6:
-                playingField[4][2] = " /˙\\";
+                board[4][2] = " /˙\\";
                 break;
-
         }
     } // считает ошибки
 
-    public static void show() {
-        for (int i = 0; i < playingField.length; i++) {
-            for (int j = 0; j < playingField[i].length; j++) {
-                System.out.print(playingField[i][j]);
+    public static void showBoard() {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                System.out.print(board[i][j]);
             }
             System.out.println();
-
         }
-        //System.out.println(word);
-
     } // показывает поле
 
-    public static void restart() {
-        initial = 0;
-        good = 0;
-        nev = 1;
-        for (int i = 0; i < playingField.length; i++) {
-            for (int j = 0; j < playingField[i].length; j++) {
-                playingField[i][j] = playingFieldRestart[i][j];
+    public static void startNewGame() {  //при новой игре нужно обнулить счетчики (кроме статистики) и измененное поле
+        mistakesCount = 0;
+        correctLettersCount = 0;
+        wrongLettersCount = 1;
+        for (int i = 0; i < board.length; i++) {  //очищаем поле
+            for (int j = 0; j < board[i].length; j++) {
+                board[i][j] = resetBoard[i][j];
             }
         }
-        word = randomWord();
-
+        secretWord = getRandomWord(); //генерируем новое слово
     }
 
-    static String[][] playingFieldRestart = {{"__", "__", "_ "},
+    static String[][] resetBoard = {{"__", "__", "_ "},  // шаблон для расчистки поля для новой игры
             {"| ", "  ", " |"},
             {"| ", "  ", "  "},
             {"| ", "  ", "   "},
@@ -237,19 +220,4 @@ public class Stars {
             {" "},
             {" ", " ", " ", " ", " ", " ", " ", " ", " ", " "},
             {"Неверно введенные буквы :", "", "", "", "", "", ""}};
-
 }
-
-
-/*class ku{
-    {{"__", "__", "_ "},
-        {"| ", "  ", " |"},
-        {"| ", "  ", " 0" },
-        {"| ", "  ", "/|\\"},
-        {"| ", " ", " /˙\\"},
-        {"A ", "  ", "  "},
-        {" "},
-        {" "," "," "," "," "," "," "," "," "," "}};
-}
-
- */
