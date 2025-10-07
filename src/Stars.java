48d9497547763db1a9aaece89c83dfd8db877932
codleto <codleto@gmail.com>
2025-10-07 14:34:40 +0300

    fix: rename

--- src/Stars.java
+++ src/Stars.java
@@ -2,7 +2,7 @@
 import java.util.Random;
 import java.util.Scanner;
 
-public class Hangman {
+public class Stars {
     public static void main(String[] args) {
         do {
             showStartMenu();
