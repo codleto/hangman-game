3de0cb14f6d047532b33c2eafd6c5f2e1ef946f8
codleto <codleto@gmail.com>
2025-10-10 13:12:34 +0300

    refactor(structure): reorder methods to conventional layout

--- src/main/hangman/Stars.java
+++ src/main/hangman/Stars.java
@@ -1,31 +1,5 @@
 package main.hangman;
 
-/*
-+ Создать метод вызова консольного отображения меню
-
-- Пустой ввод ломает игровой процесс
-- Буква "ё" не считается за кириллицу, сделать чтобы и строчные и прописные
-- если вводишь цифры то говорить что нельзя вводить цифры или типа больше одной цифры
-- сделать файл словаря
-- повторно использованная неправильная буква недолжна увеличивать счетчик
-- сделать чтобы писало в консоли что эту буква уже угадана
-- сделать чтобы после выхода статистика ( создание файла куда идет запись) не сбрасывалась и добавить кнопку сброса статистики
-- во всех ответвлениях где есть сканнер нужно условно продублировать защиту от ошибок ввода
-- убрать коменты
-- лучше всегда использовать while вместо do в наших примерах в основном нужен бесконечный цикл выход из него брейк,
- если вошли в какой то иф то континье
-- каждый метод должен выполнять одну задачу а не несколько как в showStartMenu где и валидация ввода и прочее
-- сделать маскировку слов * для прикола
-
-
-+ в мейне не нужен цикл, он бесмысленный , нам просто нужно запустить метод и все
-
-- отдельно сделать метод где пользователь вводит
-
-- как я понимаю список хуевых букв можно сделать через Set чтобы буквы были уникальные чтобы не множить счетчик ошибок
- */
-
-//
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -34,19 +8,18 @@ import java.util.*;
 
 
 public class Stars {
-    //СЧЕТЧИКИ
+
+    public static final Scanner scanner = new Scanner(System.in);
+
     public static List<String> wordMask = new LinkedList<>();
-
     public static Set<String> correctLetters = new HashSet<>();
-
-    public static String secretWord = chooseRandomWord();
-
     public static Set<String> wrongLetters = new HashSet<>();
 
-    public static int wins = 0; // счетчик выигрышей
-    public static int losses = 0; // счетчик проигрышей
+    public static  String secretWord = chooseRandomWord();
 
-    // Поле
+    public static int wins = 0;
+    public static int losses = 0;
+
     public static String[] hangmanStages = {
             """
            +---+
@@ -119,8 +92,106 @@ public class Stars {
             
             """
     };
-    public static Scanner scanner = new Scanner(System.in);
-    // так как все методы одинаковые то встраиваем их по логике
+    public static void main(String[] args) {
+        launchGame();
+    }
+
+    public static void launchGame() {
+
+        while (true) {
+            showMenu();
+            int ch = readMenuOption();
+            if (ch == 1) {
+                resetGame();
+                startGame();
+            } else if (ch == 2) {
+                statistic();
+                System.out.println("0 - Выход");
+                while (true) {
+                    int exit = readMenuOption();
+                    if (exit == 0) {
+                        break;
+                    }
+                    System.out.println("Введите 0 для выхода");
+                }
+            } else if (ch == 3) {
+                System.out.println("Выход");
+                break;
+            } else {
+                System.out.println("Ошибка! Введите 1, 2 или 3");
+            }
+        }
+    }
+
+    public static void showMenu() {
+        System.out.println("1 - Новая игра");
+        System.out.println("2 - Статистика");
+        System.out.println("3 - Выход");
+    }
+
+    public static void startGame() {
+        initMask();
+        while (true) {
+            showBoard();
+            String s = readCyrillicWord();
+            openLetters(s);
+            registerWrongLetter(s);
+            if (checkWin()) {
+                showBoard();
+                System.out.println("ТЫ ВЫИГРАЛ!!!");
+                wins++;
+                break;
+            }
+            if (checkLoss()) {
+                losses++;
+                System.out.println("Вы проиграли!");
+                break;
+            }
+        }
+    }
+
+    public static boolean checkWin() {
+        return !wordMask.contains("*");
+    }
+
+    public static boolean checkLoss() {
+        return wrongCount() == 6;
+    }
+
+    public static void resetGame() {
+        wordMask.clear();
+        wrongLetters.clear();
+        correctLetters.clear();
+        secretWord = chooseRandomWord();
+    }
+
+    public static void statistic() {
+        if (wins == 0 && losses == 0) {
+            System.out.println("Ты еще не сыграл, статистики нет ;)");
+        } else {
+            System.out.println("Твоя статистика!");
+            System.out.println("Выигрыши: " + wins);
+            System.out.println("Проигрыши: " + losses);
+        }
+    }
+
+    public static void showBoard() {
+        String a = hangmanStages[wrongCount()];
+        System.out.println(a);
+        printScoreboard();
+    }
+
+    public static void printScoreboard(){
+        for(String x : wordMask){
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        System.out.print("Ошибки:" + "(" + wrongLetters.size() + ") ");
+        for(String y : wrongLetters){
+            System.out.print(y + " ");
+        }
+        System.out.println();
+    }
 
     public static int readMenuOption() {
         while (true) {
@@ -139,16 +210,6 @@ public class Stars {
                 return s.toLowerCase();
             }
             System.out.println("Ошибка! Введите ОДНУ букву кириллицы без цифр и символов!");
-        }
-    }
-
-    public static void statistic() {
-        if (wins == 0 && losses == 0) {
-            System.out.println("Ты еще не сыграл, статистики нет ;)");
-        } else {
-            System.out.println("Твоя статистика!");
-            System.out.println("Выигрыши: " + wins);
-            System.out.println("Проигрыши: " + losses);
         }
     }
 
@@ -186,18 +247,6 @@ public class Stars {
         }
     }
 
-    public static void printScoreboard(){
-        for(String x : wordMask){
-            System.out.print(x + " ");
-        }
-        System.out.println();
-        System.out.print("Ошибки:" + "(" + wrongLetters.size() + ") ");
-        for(String y : wrongLetters){
-            System.out.print(y + " ");
-        }
-        System.out.println();
-    }// это по факту панель показа слова и ошибок
-
     public static void openLetters(String a) {
         String letter = a.substring(0, 1).toLowerCase().trim();
 
@@ -227,84 +276,5 @@ public class Stars {
 
     public static int wrongCount(){
         return wrongLetters.size();
-    }
-
-    public static void showBoard() {
-        String a = hangmanStages[wrongCount()];
-        System.out.println(a);
-        printScoreboard();
-    }
-
-    public static void launchGame() {
-        //Меню в начале игры
-        while (true) {
-            showMenu();
-            int ch = readMenuOption();
-            if (ch == 1) { // если выбрали 1 - начинаем новую игру
-                resetGame();
-                startGame();
-            } else if (ch == 2) {// если 2 - смотрим статистику//
-                statistic();
-                System.out.println("0 - Выход");
-                while (true) {
-                    int exit = readMenuOption();
-                    if (exit == 0) {
-                        break;
-                    }
-                    System.out.println("Введите 0 для выхода");
-                }
-            } else if (ch == 3) {
-                System.out.println("Выход");
-                break;
-            } else {
-                System.out.println("Ошибка! Введите 1, 2 или 3");
-            }
-        }
-    }
-
-    public static void showMenu() {
-        System.out.println("1 - Новая игра");
-        System.out.println("2 - Статистика");
-        System.out.println("3 - Выход");
-    }
-
-    public static void startGame() {//--------- ---------------исправили двойное инфо табло
-        initMask();
-        while (true) {
-            showBoard();
-            String s = readCyrillicWord();
-            openLetters(s);
-            registerWrongLetter(s);
-            if (checkWin()) {
-                showBoard();
-                System.out.println("ТЫ ВЫИГРАЛ!!!");
-                wins++;
-                break;
-            }
-            if (checkLoss()) {
-                losses++;
-                System.out.println("Вы проиграли!");
-                break;
-            }
-        }
-    }
-
-    public static boolean checkWin() {
-        return !wordMask.contains("*");
-    }
-
-    public static boolean checkLoss() {  //проверка проиграли мы или нет
-        return wrongCount() == 6;
-    }
-
-    public static void resetGame() {
-        wordMask.clear();
-        wrongLetters.clear();
-        correctLetters.clear();
-        secretWord = chooseRandomWord();
-    }
-
-    public static void main(String[] args) {
-        launchGame();
     }
 }
